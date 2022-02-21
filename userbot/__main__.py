74bb0713c67f20766310ef8ecb8aa65f13866bff
Yansen <97857681+Yansensad@users.noreply.github.com>
2022-02-21 12:27:11 +0700

    Add files via upload

---
+++ userbot/__main__.py
@@ -0,0 +1,30 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# you may not use this file except in compliance with the License.
+#
+""" Userbot start point """
+
+import sys
+from importlib import import_module
+
+from userbot import BOT_VER, LOGS, bot
+from userbot.modules import ALL_MODULES
+from userbot.utils.tools import ya_kali_ngga
+
+
+try:
+    for module_name in ALL_MODULES:
+        imported_module = import_module("userbot.modules." + module_name)
+    bot.start()
+    LOGS.info(f"⚡Yansen-Userbot⚡ ⚙️ V{BOT_VER} [ TELAH DIAKTIFKAN! ]")
+except BaseException as e:
+    LOGS.info(str(e), exc_info=True)
+    sys.exit(1)
+
+
+bot.loop.run_until_complete(ya_kali_ngga())
+if len(sys.argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.run_until_disconnected()
