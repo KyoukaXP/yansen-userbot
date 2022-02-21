0c7ca68561a54d6a4ec4fd375697d38ca0c48b5b
Yansen <97857681+Yansensad@users.noreply.github.com>
2022-02-22 00:38:15 +0700

    Create cping.py

---
+++ userbot/modules/cping.py
@@ -0,0 +1,51 @@
+# OWN MY CODE RENDY
+# Credits Rendy
+
+""" Userbot module containing commands related to the \
+    Information Superhighway (yes, Internet). """
+
+import random
+from userbot.events import register
+from userbot import DEVS
+
+# OWN BY RENDY CODE
+# CPING
+
+cping = [
+    "**The Power Of Yansen Userbot** `100.699` ",
+    "**Duar Memek** `999.999` ",
+    "**Duar Petir Zeus** `565.930` ",
+    "**Jackpot** `789.666` ",
+]
+
+
+async def get_readable_time(seconds: int) -> str:
+    count = 0
+    up_time = ""
+    time_list = []
+    time_suffix_list = ["Dtk", "Mnt", "Jam", "Hari"]
+
+    while count < 4:
+        count += 50
+        remainder, result = divmod(
+            seconds, 60) if count < 3 else divmod(
+            seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        up_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    up_time += ":".join(time_list)
+
+    return up_time
+
+
+@register(incoming=True, from_users=DEVS, pattern=r"^.cping$")
+async def _(rendy):
+    await rendy.reply(random.choice(cping))
