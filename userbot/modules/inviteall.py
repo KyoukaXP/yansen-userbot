f8fc75484525040ac414276d7df2cfbf80570425
Yansen <97857681+Yansensad@users.noreply.github.com>
2022-03-04 20:27:15 +0700

    lol

--- userbot/modules/inviteall.py
+++ userbot/modules/inviteall.py
@@ -12,7 +12,7 @@ from telethon.tl.functions.channels import GetFullChannelRequest
 from telethon.tl.functions.messages import GetFullChatRequest
 
 from userbot import CMD_HANDLER as cmd
-from userbot import CMD_HELP
+from userbot import CMD_HELP, DEVS
 from userbot.events import register
 from userbot.utils import lepin_cmd
 
