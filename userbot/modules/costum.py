ebca414e8d5bfeb6e02cc8b2ea81e20c173a1228
Yansen <97857681+Yansensad@users.noreply.github.com>
2022-03-04 07:20:15 +0700

    y

--- userbot/modules/costum.py
+++ userbot/modules/costum.py
@@ -11,10 +11,11 @@
 """ Userbot module containing commands for keeping costum global notes. """
 
 from userbot import BOTLOG_CHATID, CMD_HELP
-from userbot.events import register
+from userbot import CMD_HANDLER as cmd
+from userbot.utils import lepin_cmd
 
 
-@register(outgoing=True, pattern=r"\.\w*", ignore_unsafe=True, disable_errors=True)
+@lepin_cmd(pattern="\.\w*", ignore_unsafe=True, disable_errors=True)
 async def on_snip(event):
     """costums logic."""
     try:
@@ -45,7 +46,7 @@ async def on_snip(event):
             await event.delete()
 
 
-@register(outgoing=True, pattern=r"^\.costum (\w*)")
+@lepin_cmd(pattern="costum$")
 async def on_snip_save(event):
     """For .costum command, saves costums for future use."""
     try:
@@ -86,7 +87,7 @@ async def on_snip_save(event):
         await event.edit(success.format("Berhasil", keyword))
 
 
-@register(outgoing=True, pattern=r"^\.costums$")
+@lepin_cmd(pattern="costums$")
 async def on_snip_list(event):
     """For .costums command, lists costums saved by you."""
     try:
@@ -104,7 +105,7 @@ async def on_snip_list(event):
     await event.edit(message)
 
 
-@register(outgoing=True, pattern=r"^\.delcostum (\w*)")
+@lepin_cmd(pattern="delcostum$")
 async def on_snip_delete(event):
     """For .delcostum command, deletes a costum."""
     try:
@@ -122,11 +123,11 @@ async def on_snip_delete(event):
 CMD_HELP.update(
     {
         "costum": "**Plugin : **`costum`\
-        \n\n  •  **Syntax :** `.costum` <nama> <data> atau membalas pesan dengan .costum <nama>\
+        \n\n  •  **Syntax :** `{cmd}costum` <nama> <data> atau membalas pesan dengan .costum <nama>\
         \n  •  **Function : **Menyimpan pesan costum (catatan global) dengan nama. (bisa dengan gambar, docs, dan stickers!)\
-        \n\n  •  **Syntax :** `.costums`\
+        \n\n  •  **Syntax :** `{cmd}costums`\
         \n  •  **Function : **Mendapat semua costums yang disimpan.\
-        \n\n  •  **Syntax :** `.delcostum` <nama_costum>\
+        \n\n  •  **Syntax :** `{cmd}delcostum` <nama_costum>\
         \n  •  **Function : **Menghapus costum yang ditentukan.\
     "
     }
