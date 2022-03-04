7ea136a01e2f6320b91da1eb106cd3dd63d8bb11
Yansen <97857681+Yansensad@users.noreply.github.com>
2022-03-04 07:09:18 +0700

    y

--- userbot/modules/__help.py
+++ userbot/modules/__help.py
@@ -2,6 +2,7 @@ import logging
 
 from userbot import BOT_USERNAME
 from userbot.utils import lepin_cmd
+from userbot import CMD_HANDLER as cmd
 
 logging.basicConfig(
     format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.WARNING
@@ -13,7 +14,7 @@ async def yardim(event):
     try:
         tgbotusername = BOT_USERNAME
         if tgbotusername is not None:
-            results = await event.client.inline_query(tgbotusername, "@skyzusupport")
+            results = await event.client.inline_query(tgbotusername, "@trashme2")
             await results[0].click(
                 event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
             )
