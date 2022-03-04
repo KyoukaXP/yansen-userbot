467facd52a9edb52a5c57cdc92959b149658552e
LepinBot <airabaqdal057@gmail.com>
2022-03-04 11:01:55 +0000

    Yansen-Userbot: auto fixes
    Signed-off-by: LepinBot <airabaqdal057@gmail.com>

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -31,7 +31,7 @@ try:
     LOGS.info(
         f"Jika Reponya Bermasalah Silahkan Lapor Ke @lepinex. [ BOT TELAH DIAKTIFKAN! ]"
     )
-    LOGS.info( f"🍻Yansen-Userbot Telah Di Aktifkan {BOT_VER}" )
+    LOGS.info(f"🍻Yansen-Userbot Telah Di Aktifkan {BOT_VER}")
 except BaseException as e:
     LOGS.info(str(e), exc_info=True)
     sys.exit(1)
