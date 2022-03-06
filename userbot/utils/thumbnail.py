f52e902510b01e51221038691b65749033694d47
Yansen <97857681+Yansensad@users.noreply.github.com>
2022-03-06 21:35:16 +0700

    Update thumbnail.py

--- userbot/utils/thumbnail.py
+++ userbot/utils/thumbnail.py
@@ -21,7 +21,9 @@ async def gen_thumb(thumbnail, title, userid, ctitle):
     async with aiohttp.ClientSession() as session:
         async with session.get(thumbnail) as resp:
             if resp.status == 200:
-                f = await aiofiles.open(f"resources/thumb{userid}.png", mode="wb")
+                f = await aiofiles.open(
+                   f"resources/thumb{userid}.png", mode="wb"
+                    )
                 await f.write(await resp.read())
                 await f.close()
     theme = random.choice(themes)
