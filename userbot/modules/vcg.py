e438e8ae87bfb16f3e7e696ec010e0b1ded81aa7
Yansen <97857681+Yansensad@users.noreply.github.com>
2022-03-16 04:45:23 +0700

    Update vcg.py

--- userbot/modules/vcg.py
+++ userbot/modules/vcg.py
@@ -11,7 +11,7 @@ from telethon.utils import get_display_name
 
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
-from userbot.utils import skyzu_cmd
+from userbot.utils import lepin_cmd
 
 NO_ADMIN = "`Maaf Kamu Bukan Admin 👮`"
 
@@ -34,7 +34,7 @@ def user_list(l, n):
         yield l[i : i + n]
 
 
-@skyzu_cmd(pattern="startvc$")
+@lepin_cmd(pattern="startvc$")
 async def start_voice(c):
     chat = await c.get_chat()
     admin = chat.admin_rights
@@ -50,7 +50,7 @@ async def start_voice(c):
         await c.edit(f"**ERROR:** `{ex}`")
 
 
-@skyzu_cmd(pattern="stopvc$")
+@lepin_cmd(pattern="stopvc$")
 async def stop_voice(c):
     chat = await c.get_chat()
     admin = chat.admin_rights
@@ -66,7 +66,7 @@ async def stop_voice(c):
         await c.edit(f"**ERROR:** `{ex}`")
 
 
-@skyzu_cmd(pattern="vcinvite")
+@lepin_cmd(pattern="vcinvite")
 async def _(sky):
     await sky.edit("`Sedang Menginvite Member...`")
     users = []
