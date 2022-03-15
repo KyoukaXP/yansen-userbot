55df854548b58909f1f558ab7b8dc27c68106de0
Yansen <97857681+Yansensad@users.noreply.github.com>
2022-03-16 04:07:02 +0700

    Add files via upload

---
+++ userbot/modules/vcg.py
@@ -0,0 +1,96 @@
+# Thanks TeamUltroid
+# Ported By @skyzu
+
+from telethon.tl import types
+from telethon.tl.functions.channels import GetFullChannelRequest as getchat
+from telethon.tl.functions.phone import CreateGroupCallRequest as startvc
+from telethon.tl.functions.phone import DiscardGroupCallRequest as stopvc
+from telethon.tl.functions.phone import GetGroupCallRequest as getvc
+from telethon.tl.functions.phone import InviteToGroupCallRequest as invitetovc
+from telethon.utils import get_display_name
+
+from userbot import CMD_HANDLER as cmd
+from userbot import CMD_HELP
+from userbot.utils import skyzu_cmd
+
+NO_ADMIN = "`Maaf Kamu Bukan Admin 👮`"
+
+
+def vcmention(user):
+    full_name = get_display_name(user)
+    if not isinstance(user, types.User):
+        return full_name
+    return f"[{full_name}](tg://user?id={user.id})"
+
+
+async def get_call(sky):
+    sky = await sky.client(getchat(sky.chat_id))
+    await sky.client(getvc(sky.full_chat.call, limit=1))
+    return hehe.call
+
+
+def user_list(l, n):
+    for i in range(0, len(l), n):
+        yield l[i : i + n]
+
+
+@skyzu_cmd(pattern="startvc$")
+async def start_voice(c):
+    chat = await c.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
+
+    if not admin and not creator:
+        await c.edit(f"**Maaf {ALIVE_NAME} Bukan Admin 👮**")
+        return
+    try:
+        await c.client(startvc(c.chat_id))
+        await c.edit("`Memulai Obrolan Suara`")
+    except Exception as ex:
+        await c.edit(f"**ERROR:** `{ex}`")
+
+
+@skyzu_cmd(pattern="stopvc$")
+async def stop_voice(c):
+    chat = await c.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
+
+    if not admin and not creator:
+        await c.edit(f"**Maaf {ALIVE_NAME} Bukan Admin 👮**")
+        return
+    try:
+        await c.client(stopvc(await get_call(c)))
+        await c.edit("`Mematikan Obrolan Suara`")
+    except Exception as ex:
+        await c.edit(f"**ERROR:** `{ex}`")
+
+
+@skyzu_cmd(pattern="vcinvite")
+async def _(sky):
+    await sky.edit("`Sedang Menginvite Member...`")
+    users = []
+    z = 0
+    async for x in sky.client.iter_participants(sky.chat_id):
+        if not x.bot:
+            users.append(x.id)
+    hmm = list(user_list(users, 6))
+    for p in hmm:
+        try:
+            await sky.client(invitetovc(call=await get_call(sky), users=p))
+            z += 6
+        except BaseException:
+            pass
+    await sky.edit(f"`Menginvite {z} Member`")
+
+
+CMD_HELP.update(
+    {
+        "vcg": f"𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}startvc`\
+         \n↳ : Memulai Obrolan Suara dalam Group.\
+         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}stopvc`\
+         \n↳ : `Menghentikan Obrolan Suara Pada Group.`\
+         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}vcinvite`\
+         \n↳ : Invite semua member yang berada di group."
+    }
+)
---
+++ userbot/modules/xgban.py
@@ -0,0 +1,48 @@
+from asyncio import sleep
+
+from telethon.tl.functions.channels import EditBannedRequest
+from telethon.tl.types import ChatBannedRights
+
+from userbot import CMD_HANDLER as cmd
+from userbot import CMD_HELP
+from userbot.utils import skyzu_cmd
+
+
+# Port By @skyzu
+# Perkontolan Dengan Hapus Credits
+@skyzu_cmd(pattern="allban(?: |$)(.*)")
+async def testing(event):
+    nikal = await event.get_chat()
+    chutiya = await event.client.get_me()
+    admin = nikal.admin_rights
+    creator = nikal.creator
+    if not admin and not creator:
+        await event.edit("Anda Tidak Mempunyai Hak")
+        return
+    await event.edit("Tidak Melakukan Apa-apa")
+    # Thank for Dark_Cobra
+    everyone = await event.client.get_participants(event.chat_id)
+    for user in everyone:
+        if user.id == chutiya.id:
+            pass
+        try:
+            await event.client(
+                EditBannedRequest(
+                    event.chat_id,
+                    int(user.id),
+                    ChatBannedRights(until_date=None, view_messages=True),
+                )
+            )
+        except Exception as e:
+            await event.edit(str(e))
+        await sleep(0.5)
+    await event.edit("Tidak Ada yang Terjadi di sini🙃🙂")
+
+
+CMD_HELP.update(
+    {
+        "allban": f"**Plugin : **`allban`\
+    \n\n**Syntax : **`{cmd}allban`\
+    \n**Function : **ban all members in 1 cmnd"
+    }
+)
