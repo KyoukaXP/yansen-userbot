b355552d6ae1c26101b84acc5351cf99b71a5253
Yansen <97857681+Yansensad@users.noreply.github.com>
2022-03-16 06:17:53 +0700

    Update deploy.py

--- userbot/modules/deploy.py
+++ userbot/modules/deploy.py
@@ -4,14 +4,14 @@ from platform import uname
 from userbot import ALIVE_NAME
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
-from userbot.utils import skyzu_cmd
+from userbot.utils import lepin_cmd
 
 # ================= CONSTANT =================
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 # ============================================
 
 
-@skyzu_cmd(pattern="deploy ?(.*)")
+@lepin_cmd(pattern="deploy ?(.*)")
 async def _(event):
 
     if event.fwd_from:
@@ -44,10 +44,10 @@ async def _(event):
         "telethon.network.mtprotosender -\nINFO - Connecting to 91.108.56.146:443/TcpFull...",
         "telethon.network.mtprotosender -\nINFO - Connection to 91.108.56.146:443/TcpFull complete!",
         "telethon.network.mtprotosender -\nINFO - Received response without parent request",
-        "INFO - Skyzu-Userbot: Logged in as 557667062",
-        "INFO - Skyzu-Userbot: Successfully...",
-        "919852+00:00 app[worker.1]: 919 - Rose-Userbot -",
-        "INFO - ⚡ Skyzu-Userbot ⚡ ⚙️ V5.0 [TELAH DIAKTIFKAN!]",
+        "INFO - Yansen-Userbot: Logged in as 557667062",
+        "INFO - Yansen-Userbot: Successfully...",
+        "919852+00:00 app[worker.1]: 919 - Yansen-Userbot -",
+        "INFO - ⚡ Yansen-Userbot ⚡ ⚙️ V8.0 [TELAH DIAKTIFKAN!]",
         "**Build Succeeded**",
     ]
 
